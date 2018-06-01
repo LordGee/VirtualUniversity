9c61a3381e50ef62064e177c9d30f1784a3f1740
LordGee <gordon.johnson@mychaos.co.uk>
2018-06-01 10:50:56 +0100

    Attempts to stop the test boy from flying

--- vu_rpg/Assets/AssetStore/uMMORPG/Scripts/UnityVersionCheck.cs
+++ vu_rpg/Assets/AssetStore/uMMORPG/Scripts/UnityVersionCheck.cs
@@ -5,7 +5,7 @@ public class UnityVersionCheck : MonoBehaviour {
     public string download = "https://unity3d.com/unity/qa/lts-releases";
 
     void Awake() {
-        if (Application.unityVersion != recommended)
-            Debug.LogWarning("uMMORPG works best with Unity " + recommended + " LTS! Download: " + download + "\n");
+        //if (Application.unityVersion != recommended)
+            // Debug.LogWarning("uMMORPG works best with Unity " + recommended + " LTS! Download: " + download + "\n");
     }
 }
